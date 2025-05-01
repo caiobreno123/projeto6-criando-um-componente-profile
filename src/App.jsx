c9b53de100157cd2c2991e099a63fd606f375e1b
caiobreno123 <caiobrenopiresduarte@gmail.com>
2025-05-01 20:43:38 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Perfil from "./components/Profile"
 export default function App() {
   return (
     <div className="App">
-      <Perfil nome='Helder Lima' idade='25' profissao='Trader' foto={"https://th.bing.com/th/id/OIP.mkzAbppmkgJBUlx0iEil3wHaHa?w=183&h=183&c=7&r=0&o=5&pid=1.7"} />
-      <Perfil nome='José Enzo' idade='23' profissao='Programador' foto={"https://static.vecteezy.com/system/resources/previews/012/591/917/original/programmer-icon-illustration-vector.jpg"} />
-      <Perfil nome='Francisco Tiago' idade='27' profissao='Comerciante' foto={"https://static.vecteezy.com/system/resources/previews/006/675/277/non_2x/trade-line-icon-vector.jpg"} />
+      <Perfil nome='Carlos antonio' idade='22' profissao='Trader' foto={"https://th.bing.com/th/id/OIP.mkzAbppmkgJBUlx0iEil3wHaHa?w=183&h=183&c=7&r=0&o=5&pid=1.7"} />
+      <Perfil nome='José Enzo' idade='17' profissao='Programador' foto={"https://static.vecteezy.com/system/resources/previews/012/591/917/original/programmer-icon-illustration-vector.jpg"} />
+      <Perfil nome='Francisco cavalcante' idade='33' profissao='Comerciante' foto={"https://static.vecteezy.com/system/resources/previews/006/675/277/non_2x/trade-line-icon-vector.jpg"} />
     </div>
   )
 }
